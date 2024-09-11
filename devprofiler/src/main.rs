2597b8cdc2db406070266120fe5a809f1b51ef1c
Tapish Rathore <tapish@vibinex.com>
2024-09-12 02:41:17 +0530

    Update main.rs

--- devprofiler/src/main.rs
+++ devprofiler/src/main.rs
@@ -26,7 +26,7 @@ struct Cli {
     provider: Option<String>,
 	/// path scanned for repositories
     path: Option<PathBuf>,
-	//// repository name and owner
+	//// repository name and owner and repo slug
 	repo_slug: Option<String>,
 }
 
