f2a077c6cff8833afb28c4599623cccb75e76f22
Tapish Rathore <tapish@vibinex.com>
2024-09-12 06:41:59 +0530

    Update main.rs

--- devprofiler/src/main.rs
+++ devprofiler/src/main.rs
@@ -115,7 +115,7 @@ fn process_aliases(alias_vec: Vec::<String>, einfo: &mut RuntimeInfo, writer: &m
 
 fn main() {
 	let args = Cli::parse();
-	let mut dockermode = false; //dummy comment
+	let mut dockermode = false;
 	match args.provider {
 		Some(ref argval) => {
 			if argval == "github" || argval == "bitbucket" {
